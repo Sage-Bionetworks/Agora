f880972c7ff54dbef19501055c97b80d0bc1e211
Zaknarfen <ad.renan@gmail.com>
2018-05-28 19:31:59 -0300

    Adding stubs and one complete test
    	- Added stubs for the router, router outlet, router link and
    	activated route
    	- Added a test for the genes-view component

---
+++ src/app/genes/genes-view/genes-view.component.spec.ts
@@ -0,0 +1,46 @@
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
+import { DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
+import { routes } from '../genes-routing.module';
+
+import {
+    RouterStub,
+    RouterOutletStubComponent,
+    RouterLinkStubDirective
+} from '../../../app/testing';
+
+import { GenesViewComponent } from './genes-view.component';
+
+describe('Component: GenesView', () => {
+    let component: GenesViewComponent;
+    let fixture: ComponentFixture<GenesViewComponent>;
+    let router: RouterStub;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                GenesViewComponent,
+                RouterOutletStubComponent
+            ],
+            // The NO_ERRORS_SCHEMA tells the Angular compiler to ignore unrecognized
+            // elements and attributes
+            schemas: [ NO_ERRORS_SCHEMA ],
+            providers: [
+                { provide: Router, useValue: new RouterStub() }
+            ]
+        })
+        .compileComponents();
+
+        fixture = TestBed.createComponent(GenesViewComponent);
+
+        // Get the injected instances
+        router = fixture.debugElement.injector.get(Router);
+
+        component = fixture.componentInstance; // Component test instance
+    }));
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
---
+++ src/app/testing/router-stub.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { NavigationExtras } from '@angular/router';
+
+@Injectable()
+export class RouterStub {
+    navigate(commands: any[], extras?: NavigationExtras) {
+        // Empty comment so tslint does not complain about empty block
+    }
+}
