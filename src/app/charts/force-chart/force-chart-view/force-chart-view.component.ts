32e710801cf9f1d7a7fb383be6b194da7a77c3ee
Diego Ferreyra <diego.ferreyra@toptal.com>
2018-07-12 12:37:22 -0300

    network size fixes

--- src/app/charts/force-chart/force-chart-view/force-chart-view.component.ts
+++ src/app/charts/force-chart/force-chart-view/force-chart-view.component.ts
@@ -60,15 +60,14 @@ export class ForceChartViewComponent implements AfterViewInit, OnChanges {
 
     ngAfterViewInit() {
         this.width = this.forceChart.nativeElement.parentElement.offsetWidth;
-        this.height = this.forceChart.nativeElement.offsetHeight;
+        this.height = this.forceChart.nativeElement.offsetParent.offsetHeight;
         this.loaded = true;
-        console.log(this.networkData);
         this.renderChart();
     }
 
     onResize(event) {
         this.width = this.forceChart.nativeElement.parentElement.offsetWidth;
-        this.height = this.forceChart.nativeElement.offsetHeight;
+        this.height = this.forceChart.nativeElement.offsetParent.offsetHeight;
         this.forceChart.nativeElement.children[0].setAttribute('width', this.width);
         this.forceChart.nativeElement.children[0].setAttribute('height', this.height);
 
@@ -76,7 +75,7 @@ export class ForceChartViewComponent implements AfterViewInit, OnChanges {
         this.simulation.force('charge', d3.forceManyBody());
         this.simulation.force('link', d3.forceLink(this.networkData.links)
             .links(this.networkData.links).id((d: any) => d.id)
-            .distance(this.width / 3.5))
+            .distance(this.width / 3))
             .force('collide', d3.forceCollide()
                 .radius(18)
                 .strength(0.5))
@@ -193,7 +192,7 @@ export class ForceChartViewComponent implements AfterViewInit, OnChanges {
 
         this.simulation.force('link', d3.forceLink(this.networkData.links)
             .links(this.networkData.links).id((d: GeneNode) => d.id)
-            .distance(this.width / 3.5))
+            .distance(this.width / 3))
             .force('collide', d3.forceCollide()
                 .radius(18)
                 .strength(0.5));
@@ -201,7 +200,7 @@ export class ForceChartViewComponent implements AfterViewInit, OnChanges {
         const dragDrop = d3.drag()
                     .on('start', (node: any) => {
                         if (!d3.event.active) {
-                            this.simulation.alphaTarget(0.3).restart();
+                            this.simulation.alphaTarget(0).restart();
                         }
                         node.fx = node.x;
                         node.fy = node.y;
@@ -210,7 +209,7 @@ export class ForceChartViewComponent implements AfterViewInit, OnChanges {
                             .style('opacity', 0);
                     })
                     .on('drag', (node: any) => {
-                        this.simulation.alphaTarget(0.01).restart();
+                        this.simulation.alphaTarget(0).restart();
                         node.fx = d3.event.x;
                         node.fy = d3.event.y;
                         tooltip
@@ -218,7 +217,7 @@ export class ForceChartViewComponent implements AfterViewInit, OnChanges {
                     })
                     .on('end', (node: any) => {
                         if (!d3.event.active) {
-                            this.simulation.alphaTarget(.01);
+                            this.simulation.alphaTarget(0);
                         }
                         node.fx = null;
                         node.fy = null;
