7de71c8d55689ce0a17e259b4668c642d6f377bf
kdaily <kenneth.daily@sagebase.org>
2018-07-12 16:52:20 -0700

    add getting images

--- get-data-local.sh
+++ get-data-local.sh
@@ -1,3 +1,8 @@
+mkdir ../data/
+
 synapse cat syn13363290 | tail -n +2 | while IFS=, read -r id version; do
   synapse get --downloadLocation ../data/ -v $version $id ;
 done
+
+mkdir ../data/team_images/
+synapse -u ${synapseusername} -p ${synapsepassword} get -r --downloadLocation ../data/team_images/ syn12861877
