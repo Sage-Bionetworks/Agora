2b3f5f69d916a1509a1f186778d12209c3f22a62
kdaily <kenneth.daily@sagebase.org>
2018-07-13 09:38:26 -0700

    fix cleanup and making directories

--- get-data-aws.sh
+++ get-data-aws.sh
@@ -1,11 +1,12 @@
 synapseusername=`aws secretsmanager --region us-east-1 --output text get-secret-value --secret-id synapse-agora-username | cut -f 4`
 synapsepassword=`aws secretsmanager --region us-east-1 --output text get-secret-value --secret-id synapse-agora-password | cut -f 4`
 
-mkdir -f ../data/
+[ -d ../data/ ] || mkdir ../data/
 
 synapse -u ${synapseusername} -p ${synapsepassword} cat syn13363290 | tail -n +2 | while IFS=, read -r id version; do
   synapse -u ${synapseusername} -p ${synapsepassword} get --downloadLocation ../data/ -v $version $id ;
 done
 
-mkdir -f ../data/team_images/
+[ -d ../data/team_images/ ] || mkdir ../data/team_images/
+
 synapse -u ${synapseusername} -p ${synapsepassword} get -r --downloadLocation ../data/team_images/ syn12861877
--- get-data-local.sh
+++ get-data-local.sh
@@ -1,8 +1,7 @@
-mkdir -f ../data/
-
+[ -d ../data/ ] || mkdir ../data/
 synapse cat syn13363290 | tail -n +2 | while IFS=, read -r id version; do
   synapse get --downloadLocation ../data/ -v $version $id ;
 done
 
-mkdir -f ../data/team_images/
+[ -d ../data/team_images/ ] || mkdir ../data/team_images/
 synapse get -r --downloadLocation ../data/team_images/ syn12861877
