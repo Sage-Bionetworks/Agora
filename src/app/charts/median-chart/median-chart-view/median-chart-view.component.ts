11b295647a97d09b7587a5e10cadd6441f7e5756
Diego Ferreyra <diego.ferreyra@toptal.com>
2018-07-18 00:33:12 -0300

    median chart value below 0 and line on log10(5) some text and orering issues

--- src/app/charts/median-chart/median-chart-view/median-chart-view.component.ts
+++ src/app/charts/median-chart/median-chart-view/median-chart-view.component.ts
@@ -45,7 +45,8 @@ export class MedianChartViewComponent implements OnInit, AfterViewInit {
         this.ndx = crossfilter(this.geneinfo.medianexpression);
         this.group = this.ndx.groupAll();
         this.dimension = this.ndx.dimension( (d) =>  d.tissue );
-        this.tissuecoresGroup = this.dimension.group().reduceSum((d) => d.medianlogcpm);
+        this.tissuecoresGroup = this.dimension.group().reduceSum((d) =>
+        d.medianlogcpm); // > 0 ? d.medianlogcpm : 0);
     }
 
     ngAfterViewInit() {
@@ -58,9 +59,12 @@ export class MedianChartViewComponent implements OnInit, AfterViewInit {
             .height(320)
             .gap(50)
             .renderLabel(true)
+            .elasticY(false)
             .dimension(this.dimension)
             .group(this.tissuecoresGroup)
             .x(d3.scaleBand())
+            .y(d3.scaleLinear().domain([0, this.tissuecoresGroup.top(1)[0].value]))
+            .brushOn(false)
             .xUnits(dc.units.ordinal)
             .colors(['#5171C0'])
             // .yAxis().ticks(6)
@@ -68,23 +72,35 @@ export class MedianChartViewComponent implements OnInit, AfterViewInit {
                 chart.selectAll('rect').on('click', (d) => {
                     console.log('click!', d);
                 });
-                // const lefty = 0;
-                // const righty = 0; // use real statistics here!
-                // const extradata = [{ x: chart.x().range()[0], y: chart.y()(lefty) },
-                // { x: chart.x().range()[1], y: chart.y()(righty) }];
-                // const line = d3.line()
-                //     .x((d: any) =>  d.x)
-                //     .y((d: any) => 210);
-                // const chartBody = chart.select('g.chart-body');
-                // let path = chartBody.selectAll('path.extra').data([extradata]);
-                // path = path
-                //     .enter()
-                //     .append('path')
-                //     .attr('class', 'extra')
-                //     .attr('stroke', 'red')
-                //     .attr('id', 'extra-line')
-                //     .merge(path);
-                // path.attr('d', line);
+                const yDomainLength = Math.abs(this.barchart.y().domain()[1]
+                - this.barchart.y().domain()[0]);
+                // if (chart.select('rect').attr('y') === '240' ) {
+                //     chart.select('rect').attr('y', 290 );
+                // }
+                chart.selectAll('rect').each((el, i, tree) => {
+                    if (el && el.y <= 0) {
+                        tree[i].setAttribute('height', 0);
+                    }
+                 });
+                // const svgEl = (chart.selectAll('g.axis.y').node() as SVGGraphicsElement);
+                const mult = 240 / yDomainLength;
+                const lefty = 0;
+                const righty = 0; // use real statistics here!
+                const extradata = [{ x: chart.x().range()[0], y: chart.y()(lefty) },
+                { x: chart.x().range()[1], y: chart.y()(righty) }];
+                const line = d3.line()
+                    .x((d: any) =>  d.x)
+                    .y((d: any) => (Math.abs(chart.y().domain()[1] - Math.log10(5)) * mult));
+                const chartBody = chart.select('g.chart-body');
+                let path = chartBody.selectAll('path.extra').data([extradata]);
+                path = path
+                    .enter()
+                    .append('path')
+                    .attr('class', 'extra')
+                    .attr('stroke', 'red')
+                    .attr('id', 'extra-line')
+                    .merge(path);
+                path.attr('d', line);
             });
         this.barchart.render();
     }
