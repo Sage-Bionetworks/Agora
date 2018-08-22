8844a2ae784b2c0da9fb87b69b607b65dbae3b1c
Zaknarfen <ad.renan@gmail.com>
2018-08-22 18:45:54 -0300

    Improving the clean data script for Docker

--- clean-docker-data.sh
+++ clean-docker-data.sh
@@ -1,12 +1,38 @@
+containers=$(docker ps -a -q)
+exited_containers=$(docker ps -q -f status=exited)
+images=$(docker images -q)
+dangling_volumes=$(docker volume ls -qf dangling=true)
+
 # Stop all containers
-docker stop $(docker ps -a -q)
-# Delete all containers
-docker rm $(docker ps -a -q)
+if [ ! -z $container]
+then
+    docker stop $containers
+    # Delete all containers
+    docker rm $containers
+else
+    echo "No containers to stop or remove!"
+fi
+
 # Delete all images
-docker rmi $(docker images -q)
+if [ ! -z $images ]
+then
+    docker rmi $images
+else
+    echo "No images to remove!"
+fi
 
 # Remove all containers with the Exited status
-docker rm $(docker ps -q -f status=exited)
+if [ ! -z $exited_containers ]
+then
+    docker rm $exited_containers
+else
+    echo "No exited containers to remove!"
+fi
 
 # Removed all dangling volumes
-docker volume rm $(docker volume ls -qf dangling=true)
+if [ ! -z $dangling_volumes ]
+then
+    docker volume rm $dangling_volumes
+else
+    echo "No dangling volumes to remove!"
+fi
