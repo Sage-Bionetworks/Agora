f4f2be9c531f9e5bd369f15956ea88f1628e628d
Zaknarfen <ad.renan@gmail.com>
2018-08-23 14:40:46 -0300

    Adding hotfix to changes

--- clean-docker-data.sh
+++ clean-docker-data.sh
@@ -3,36 +3,37 @@ exited_containers=$(docker ps -q -f status=exited)
 images=$(docker images -q)
 dangling_volumes=$(docker volume ls -qf dangling=true)
 
-# Stop all containers
-if ! [ -z $container]
+
+if [ -z $container]
 then
+    echo "No containers to stop or remove!"
+else
+    # Stop all containers
     docker stop $containers
     # Delete all containers
     docker rm $containers
-else
-    echo "No containers to stop or remove!"
 fi
 
-# Delete all images
-if ! [ -z $images ]
+if [ -z $images ]
 then
-    docker rmi $images
-else
     echo "No images to remove!"
+else
+    # Delete all images
+    docker rmi $images
 fi
 
-# Remove all containers with the Exited status
-if ! [ -z $exited_containers ]
+if [ -z $exited_containers ]
 then
-    docker rm $exited_containers
-else
     echo "No exited containers to remove!"
+else
+    # Remove all containers with the Exited status
+    docker rm $exited_containers
 fi
 
-# Removed all dangling volumes
-if ! [ -z $dangling_volumes ]
+if [ -z $dangling_volumes ]
 then
-    docker volume rm $dangling_volumes
-else
     echo "No dangling volumes to remove!"
+else
+    # Removed all dangling volumes
+    docker volume rm $dangling_volumes
 fi
