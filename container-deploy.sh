2c393438315b43958c0fa90d5c53ff16e33d02c3
Khai Do <zaro0508@gmail.com>
2018-09-27 11:54:52 -0700

    Docker deployments to dockerhub
    Setup automated docker container deployments to the official
    Sage dockerhub repository at
    https://hub.docker.com/u/sagebionetworks/dashboard/

---
+++ container-deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+# push images to dockerhub
+docker push sagebionetworks/agora-app:latest
+docker push sagebionetworks/agora-nginx:latest
+docker push sagebionetworks/agora-mongod:latest
+docker push sagebionetworks/agora-mongo_data:latest
+docker push sagebionetworks/agora-mongo_seed_links:latest
+docker push sagebionetworks/agora-mongo_seed_info:latest
+docker push sagebionetworks/agora-mongo_seed_team:latest
+docker push sagebionetworks/agora-mongo_seed_team_images:latest
+
