e2952d2e0b3d3647ef8d8de90a2e994e35a36247
gatof81 <diego.ferreyra@toptal.com>
2018-10-31 18:55:32 -0300

    fix title test

--- src/app/app.e2e.ts
+++ src/app/app.e2e.ts
@@ -9,7 +9,7 @@ describe('App', () => {
 
   it('should have a title', async () => {
     const subject = await browser.getTitle();
-    const result  = 'Agora BETA';
+    const result  = 'Agora';
     expect(subject).toEqual(result);
   });
 
