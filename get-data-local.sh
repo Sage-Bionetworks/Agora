94d3ebf4534d357abe589ae6660374458386a4ce
Zaknarfen <ad.renan@gmail.com>
2018-11-01 14:23:40 -0300

    Small fix removing linebreak from local download script

--- get-data-local.sh
+++ get-data-local.sh
@@ -7,8 +7,7 @@ echo "Type you Synapse password"
 read synapsepassword
 
 # Version key/value should be on his own line
-DATA_VERSION=$(cat package.json | grep data-version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:spa
-ce:]]')
+DATA_VERSION=$(cat package.json | grep data-version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
 
 synapse -u ${synapseusername} -p ${synapsepassword} cat --version $DATA_VERSION syn13363290 | tail -n +2 | while IFS=, read -r id version; do
   synapse -u ${synapseusername} -p ${synapsepassword} get --downloadLocation ../data/ -v $version $id ;
