18e044bc7249e850c361e8e766d7e32e656c51f2
Zaknarfen <ad.renan@gmail.com>
2018-11-08 15:48:23 -0200

    Small UI fixes and file changes
    	- Removed docker service mongoose connection
    	- Added playback speed
    	- Removed nominated-button css class
    	- Changed assets names to make them standard
    	- Changed most pngs to svgs
    	- Changed nominated target button to .svg
    	- Closes #478

--- src/server/routes/api.ts
+++ src/server/routes/api.ts
@@ -28,13 +28,7 @@ if (process.env.MONGODB_HOST && process.env.MONGODB_PORT && process.env.APP_ENV)
         + '@' + process.env.MONGODB_HOST + ':' + process.env.MONGODB_PORT + '/agora'
         + '?authSource=admin';
 } else {
-    if (env === 'development') {
-        database.url = 'mongodb://localhost:27017/agora';
-    } else {
-        // Fallback to the service for now
-        // Service name here, not the localhost
-        database.url = 'mongodb://mongodb:27017/agora';
-    }
+    database.url = 'mongodb://localhost:27017/agora';
 }
 
 mongoose.connect(database.url, { useNewUrlParser: true });
