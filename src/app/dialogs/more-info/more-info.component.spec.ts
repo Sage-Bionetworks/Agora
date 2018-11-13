a2f3ceda90719df973ac12fe242825af83a7f36d
Zaknarfen <ad.renan@gmail.com>
2018-11-13 14:56:34 -0200

    Adding one extra unit test

---
+++ src/app/dialogs/more-info/more-info.component.spec.ts
@@ -0,0 +1,48 @@
+import {
+    async,
+    ComponentFixture,
+    TestBed
+} from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+
+import {
+    DataServiceStub
+} from '../../testing';
+
+import { MoreInfoComponent } from './';
+
+import {
+    DialogsService
+} from '../services';
+
+describe('Component: MoreInfo', () => {
+    let component: MoreInfoComponent;
+    let fixture: ComponentFixture<MoreInfoComponent>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                MoreInfoComponent
+            ],
+            // The NO_ERRORS_SCHEMA tells the Angular compiler to ignore unrecognized
+            // elements and attributes
+            schemas: [ NO_ERRORS_SCHEMA ],
+            providers: [
+                { provide: DialogsService, useValue: new DataServiceStub() }
+            ]
+        })
+        .compileComponents();
+
+        fixture = TestBed.createComponent(MoreInfoComponent);
+
+        component = fixture.componentInstance; // Component test instance
+    }));
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should have a text to display', () => {
+        expect(component.text).toEqual('View more information');
+    });
+});
