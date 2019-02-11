f169f1a48a06b9c980f6bf138d54c28fd131b8c1
Zaknarfen <ad.renan@gmail.com>
2019-02-11 18:03:21 -0200

    Adding bucket styles to druggability tab
    	- Added colored bucket img to represent different genes
    	- Changed the druggability table to look like the Figma
    	design

--- src/app/genes/gene-overview/gene-druggability/gene-druggability.component.ts
+++ src/app/genes/gene-overview/gene-druggability/gene-druggability.component.ts
@@ -34,7 +34,8 @@ export class GeneDruggabilityComponent implements OnInit {
             {
                 property: 'Small Molecule Druggability Bucket',
                 value: (this.geneInfo.druggability[0].sm_druggability_bucket === undefined) ?
-                    'No Data' : this.geneInfo.druggability[0].sm_druggability_bucket
+                    'No Data' :
+                    this.getDruggabilityText(this.geneInfo.druggability[0].sm_druggability_bucket)
             },
             {
                 property: 'Classification',
@@ -57,5 +58,92 @@ export class GeneDruggabilityComponent implements OnInit {
             { field: 'property', header: 'Property' },
             { field: 'value', header: 'Value' }
         ];
+    }
+
+    getDruggabilityTitle(bucket: number): string {
+        switch (bucket) {
+            case 1:
+                return 'SM Druggable';
+            case 2:
+            case 3:
+            case 4:
+                return 'Targetable by Homology';
+            case 5:
+            case 6:
+                return 'Probably SM Druggable';
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+            case 11:
+                return 'Potentially Targetable by Protein Family Structure';
+            case 12:
+            case 13:
+                return 'Potentially Low Ligandability';
+            case 14:
+                return 'Non-Protein Target';
+            default:
+                return '';
+        }
+    }
+
+    getDruggabilityText(bucket: number): string {
+        switch (bucket) {
+            case 1:
+                return 'Protein with a SM ligand identified from' +
+                    ' ChEMBL, meeting TCRD activity criteria';
+            case 2:
+            case 3:
+            case 4:
+                return '>=4-% homologous to a protein with' +
+                    ' a SM ligand identified from ChEMBL, meeting TCRD activity criteria';
+            case 5:
+            case 6:
+                return 'Protein with a SM ligand identified' +
+                    ' from ChEMBL data, but the ligand does not meet TCRD activity criteria';
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+            case 11:
+                return 'Is a member' +
+                ' of a gene family which has a protein member with a druggable pocket in' +
+                ' the protein structure';
+            case 12:
+            case 13:
+                return 'Has a structure but there is no' +
+                ' evidence of a druggable pocket';
+            case 14:
+                return 'Non-Protein Target<br>New modality indicated';
+            default:
+                return '';
+        }
+    }
+
+    getBucketStyle(bucket: number): any {
+        switch (bucket) {
+            case 1:
+                return '#9ACCAB';
+            case 2:
+            case 3:
+            case 4:
+                return '#F8CC7D';
+            case 5:
+            case 6:
+                return '#F1A86F';
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+            case 11:
+                return '#EFA0C5';
+            case 12:
+            case 13:
+                return '#C3C7D1';
+            case 14:
+                return '#AFDDDF';
+            default:
+                return '#FFFFFF';
+        }
     }
 }
