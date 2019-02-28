0c07159eabd5365f367ae7eb74f0b9c327bf87c7
Zaknarfen <ad.renan@gmail.com>
2019-02-28 16:30:46 -0300

    Changing error message from the http interceptor

--- src/app/shared/http-error/http-error.interceptor.ts
+++ src/app/shared/http-error/http-error.interceptor.ts
@@ -33,6 +33,7 @@ export class HttpErrorInterceptor implements HttpInterceptor {
                     let errorMessage = '';
                     let errorSummary = '';
                     let errorDetail = '';
+                    let tmpError = '';
                     if (error.error instanceof ErrorEvent) {
                         // client-side error
                         errorDetail = `Error: ${error.error.message}`;
@@ -40,19 +41,31 @@ export class HttpErrorInterceptor implements HttpInterceptor {
                     } else {
                         // server-side error
                         errorSummary = `Error Code: ${error.status}`;
-                        errorDetail = `Message: ${error.message}`;
+                        tmpError = `Message: ${error.message}`;
+                        const tmpSlashArray = tmpError.split('/');
+                        if (tmpSlashArray.length > 0) {
+                            // Get the last part of the error message string,
+                            // it should start with the gene searched id
+                            const tmpSpaceArray = tmpSlashArray[tmpSlashArray.length - 1].
+                                split[' '];
+                            if (tmpSpaceArray.length > 0) {
+                                tmpError = `Message: Gene ${tmpSlashArray[0]} not found!`;
+                            }
+                        }
+                        errorDetail = tmpError;
                     }
                     errorMessage = errorMessage + '\n' + errorSummary;
 
                     // Displays error message on screen
                     self.messageService.clear();
                     self.messageService.add({
-                        severity: 'error',
+                        severity: 'warn',
                         sticky: true,
                         summary: errorSummary,
                         detail: errorDetail,
                         life: 3000
                     });
+                    setTimeout(() => { self.messageService.clear(); }, 3000);
 
                     // Send the error message to Rollbar
                     rollbar.error(error);
