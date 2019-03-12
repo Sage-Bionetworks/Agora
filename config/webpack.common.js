3cd41604e699be645c8a369d9ec216bcb98c3275
Zaknarfen <ad.renan@gmail.com>
2019-03-12 00:22:46 -0300

    Small fixes

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -319,14 +319,7 @@ module.exports = function (options) {
         quiet: false
       }),
 
-      new PreloadWebpackPlugin({
-        rel: 'preload',
-        as(entry) {
-          if (/\.(woff|woff2|ttf|otf)$/.test(entry)) return 'font';
-        },
-        fileWhitelist: [/\.(woff|woff2|ttf|otf)$/],
-        include: 'allAssets'
-      })
+      new PreloadWebpackPlugin()
     ]],
 
     /**
