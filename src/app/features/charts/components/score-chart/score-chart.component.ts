7dd4b7278ec7fe8f99248fc40ca2b9bcddd2e830
sagely1 <114952739+sagely1@users.noreply.github.com>
2023-04-13 17:27:02 -0700

    AG-978 fix bug with charts having zero scores not displaying

--- src/app/features/charts/components/score-chart/score-chart.component.ts
+++ src/app/features/charts/components/score-chart/score-chart.component.ts
@@ -21,11 +21,11 @@ import { HelperService } from '../../../../core/services';
   styleUrls: ['./score-chart.component.scss'],
 })
 export class ScoreChartComponent extends BaseChartComponent {
-  _score = 0;
-  get score(): number {
+  _score: number | undefined;
+  get score(): number | undefined {
     return this._score;
   }
-  @Input() set score(score: number) {
+  @Input() set score(score: number | undefined) {
     this._score = score;
     this.init();
   }
@@ -37,7 +37,7 @@ export class ScoreChartComponent extends BaseChartComponent {
   override name = 'score-chart';
   dimension: any;
   group: any;
-  scoreIndex = 0;
+  scoreIndex = -1;
   break: any = {};
 
   constructor(private helperService: HelperService) {
@@ -46,7 +46,6 @@ export class ScoreChartComponent extends BaseChartComponent {
 
   override init() {
     if (
-      !this._score ||
       !this.distribution?.length ||
       !this.chartContainer?.nativeElement
     ) {
@@ -63,21 +62,23 @@ export class ScoreChartComponent extends BaseChartComponent {
 
   initData() {
     this.distribution.forEach((item: any, i: number) => {
-      if (this._score >= item.range[0] && this._score < item.range[1]) {
-        this.scoreIndex = i;
-      }
+      if (this._score !== undefined) {   
+        if (this._score >= item.range[0] && this._score < item.range[1]) {
+          this.scoreIndex = i;
+        }
 
-      // Introduce a y-axis break if this bar is huge relative to other bars
-      const minDiff = this.getMinDiff(item.value, this.distribution);
-      if (minDiff > 2000) {
-        this.break = {
-          index: i,
-          upper: Math.floor(item.value / 1000) * 1000,
-          lower: Math.ceil((item.value - minDiff) / 1000) * 1000 + 1000,
-        };
+        // Introduce a y-axis break if this bar is huge relative to other bars
+        const minDiff = this.getMinDiff(item.value, this.distribution);
+        if (minDiff > 2000) {
+          this.break = {
+            index: i,
+            upper: Math.floor(item.value / 1000) * 1000,
+            lower: Math.ceil((item.value - minDiff) / 1000) * 1000 + 1000,
+          };
 
-        this.distribution[i].truncated =
-          this.break.lower + (item.value - this.break.upper);
+          this.distribution[i].truncated =
+            this.break.lower + (item.value - this.break.upper);
+        }
       }
     });
 
@@ -117,10 +118,12 @@ export class ScoreChartComponent extends BaseChartComponent {
       .xAxis()
       .ticks(2)
       .tickFormat(d3.format('d'));
-
+      
     // Y axis
     this.chart
-      .y(d3.scaleLinear().domain([0, this.group.top(1)[0].value]))
+      .y(d3.scaleLinear()
+        .domain([0, this.group.top(1)[0].value])
+      )
       .yAxisLabel(this.yAxisLabel)
       .yAxis()
       .ticks(yTickCount)
@@ -159,9 +162,14 @@ export class ScoreChartComponent extends BaseChartComponent {
             .attr('class', 'score-label')
             .attr('x', barBox.x)
             .attr('y', barBox.y - 6)
-            .style('color', 'rgb(166 132 238)')
-            .text(this.helperService.truncateNumberToFixed(this._score, 2));
-
+            .style('color', 'rgb(166 132 238)');
+          
+          if (this._score === undefined) {
+            label.text = 'n/a';
+          }
+          else
+            label.text(this.helperService.truncateNumberToFixed(this._score, 2));
+          
           const labelBox = label.node().getBBox();
           const widthDiff = labelBox.width - barBox.width;
           label.attr(
