02b7d59cbd8315610ed6e18691a71b67cd28dea3
sagely1 <114952739+sagely1@users.noreply.github.com>
2023-10-24 13:27:37 -0700

    AG-1215 trying to remove npm ci and adding viewport code to describe block

--- tests/homepage.spec.ts
+++ tests/homepage.spec.ts
@@ -1,45 +1,49 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('http://localhost:8080');
+test.describe('specific viewport block', () => {
+  test.use({ viewport: { width: 1600, height: 1200 } });
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle('Agora | Explore Alzheimer\'s Disease Genes');
-});
+  test('has title', async ({ page }) => {
+    await page.goto('http://localhost:8080');
 
-test('has header', async ({ page }) => {
-  await page.goto('http://localhost:8080');
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle('Agora | Explore Alzheimer\'s Disease Genes');
+  });
 
-  // Expect header to be visible
-  await expect(page.locator('div#header')).toBeVisible();
-});
+  test('has header', async ({ page }) => {
+    await page.goto('http://localhost:8080');
 
-test('has heading', async ({ page }) => {
-  await page.goto('http://localhost:8080');
+    // Expect header to be visible
+    await expect(page.locator('div#header')).toBeVisible();
+  });
 
-  // Expect header to be visible
-  await expect(page.locator('h1')).toBeVisible();
-});
+  test('has heading', async ({ page }) => {
+    await page.goto('http://localhost:8080');
 
-test('has footer', async ({ page }) => {
-  await page.goto('http://localhost:8080');
+    // Expect header to be visible
+    await expect(page.locator('h1')).toBeVisible();
+  });
 
-  // Expect header to be visible
-  await expect(page.locator('#footer')).toBeVisible();
-});
+  test('has footer', async ({ page }) => {
+    await page.goto('http://localhost:8080');
 
-test('has news', async ({ page }) => {
-  // set viewport so hamburger menu doesn't show
-  page.setViewportSize({ width: 1920, height: 1080 });
+    // Expect header to be visible
+    await expect(page.locator('#footer')).toBeVisible();
+  });
 
-  await page.goto('http://localhost:8080');
+  test('has news', async ({ page }) => {
+    // set viewport so hamburger menu doesn't show
+    page.setViewportSize({ width: 1920, height: 1080 });
 
-  // Hamburger menu should be hidden for the given viewport above
-  expect(page.locator('button.header-nav-toggle')).toBeHidden();
-  
-  // look for news link and click it
-  await page.locator('a span', { hasText: 'News' }).click();
+    await page.goto('http://localhost:8080');
 
-  // title check
-  await expect(page).toHaveTitle('News | Agora Releases');
+    // Hamburger menu should be hidden for the given viewport above
+    expect(page.locator('button.header-nav-toggle')).toBeHidden();
+    
+    // look for news link and click it
+    await page.locator('a span', { hasText: 'News' }).click();
+
+    // title check
+    await expect(page).toHaveTitle('News | Agora Releases');
+  });
 });
