da434a066124fa123d5d69f24064d36780fe24a3
sagely1 <114952739+sagely1@users.noreply.github.com>
2023-11-14 12:44:40 -0800

    trying localhost for baseURL

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig, devices } from '@playwright/test';
 
-export const baseURL = 'http://127.0.0.1:8080'
+export const baseURL = 'http://localhost:8080'
 
 /**
  * Read environment variables from file.
